642430813c51a5e3490d70bbc7636606806dcb4a
sanjeev261 <156935225+sanjeev261@users.noreply.github.com>
2024-06-23 19:58:17 +0530

    Time: 4 ms (65.59%), Space: 16.1 MB (49.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,45 @@
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> result(2, -1); // Initialize result with -1 for both positions
-        if (nums.empty()) return result;
+        int n = nums.size();
 
         // Find the first occurrence of target
         int low = 0;
-        int high = nums.size() - 1;
+        int high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < target) {
-                low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
-            } else {
+            if (nums[mid] == target) {
                 if (mid == 0 || nums[mid - 1] != target) {
                     result[0] = mid;
                     break;
                 } else {
                     high = mid - 1;
                 }
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
         // Find the last occurrence of target
         low = 0;
-        high = nums.size() - 1;
+        high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < target) {
-                low = mid + 1;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
-            } else {
-                if (mid == nums.size() - 1 || nums[mid + 1] != target) {
+            if (nums[mid] == target) {
+                if (mid == n - 1 || nums[mid + 1] != target) {
                     result[1] = mid;
                     break;
                 } else {
                     low = mid + 1;
                 }
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
